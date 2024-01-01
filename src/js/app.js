90630e1faf1ebce90ea62e643c92955c726eb292
Sergius92739 <sergem279@gmail.com>
2024-01-01 23:48:46 +0300

    url fixed

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import Widget from './Widget';
 
 const widget = new Widget(
   document.getElementById('root'),
-  'https://ahj-11-1-rxjs-poling-sergius.herokuapp.com/messages/unread',
+  'https://ahj-11-1-rxjs-poling-backend.onrender.com/messages/unread',
 );
 
 widget.subscribeToUpdate();
